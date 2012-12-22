187bfbe0608c334c330478da80bad81285adcf17
ml.ohwada@gmail.com <ml.ohwada@gmail.com@d3dbfd7e-f262-9474-e871-4d1836a9463b>
2012-12-22 11:24:10 +0000

    First Version

---
+++ trunk/LodSample1/src/jp/ohwada/android/lodsample1/Constant.java
@@ -0,0 +1,9 @@
+package jp.ohwada.android.lodsample1;
+
+/**
+ * Constant
+ */
+public class Constant {
+	public final static String TAG = "LodSample1";
+    public final static boolean DEBUG = true; 	
+}
