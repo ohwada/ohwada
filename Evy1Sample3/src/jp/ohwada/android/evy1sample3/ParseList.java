20a7d7aa4b129ea911780c04023e76639e804e0a
ml.ohwada@gmail.com <ml.ohwada@gmail.com@d3dbfd7e-f262-9474-e871-4d1836a9463b>
2013-12-20 14:39:10 +0000

    FirstVersion

---
+++ Evy1Sample3/src/jp/ohwada/android/evy1sample3/ParseList.java
@@ -0,0 +1,10 @@
+package jp.ohwada.android.evy1sample3;
+
+import java.util.ArrayList;
+
+/**
+ * MIDI メッセージのリスト
+ */
+public class ParseList extends ArrayList<ParseMessage> {
+	private static final long serialVersionUID = 1L;
+}
